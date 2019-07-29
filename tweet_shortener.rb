d57a3a5fa1ce8952949148204283ac0e8c221dfe
Renée Cruz <reneebasscruz@gmail.com>
2019-07-29 16:19:06 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+require "pry"
+
+def dictionary
+ replacements = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@", 
+    "and" => "&"
+  }
+end
+
+
+def word_substitutor(tweet)
+end
